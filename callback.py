56103869638ca67dbc0808abe577658ecbe03781
Gygarte <gabriel.artemie@gmail.com>
2021-03-19 02:32:56 +0200

    removing

--- callback.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.callbacks import EarlyStopping
-
-def callback_builder():
-
-    callback = EarlyStopping(monitor="accuracy", patience= 30, restore_best_weights= True)
-
-    return callback
--- constants.py
+++
@@ -1,7 +0,0 @@
-
-TIMESTEP = 
-TRAIN_LEN = 
-VAL_LEN = 
-TEST_LEN = 
-
-def constants(TIMESTEP, )
